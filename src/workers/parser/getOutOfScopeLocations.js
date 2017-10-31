27940ebffee805d9fdc73d1f606b374ad46c7d39
Fischer.json <fischer.json@gmail.com>
2017-10-31 22:19:31 +0800

    Thoughts

--- src/workers/parser/getOutOfScopeLocations.js
+++ src/workers/parser/getOutOfScopeLocations.js
@@ -69,6 +69,45 @@ function sortByStart(a: AstLocation, b: AstLocation) {
   return 1;
 }
 
+function startsBefore(a: AstLocation, b: AstPosition) {
+  let before = a.start.line < b.line;
+  if (a.start.line === b.line) {
+    before =
+      a.start.column >= 0 && b.column >= 0 ? a.start.column <= b.column : true;
+  }
+  return before;
+}
+
+function testtt(locations: AstLocation[], position: AstPosition) {
+  let nearestPos = -1;
+  for (let i = 0; i < locations.length; i++) {
+    let loc = locations[i];
+    if (containsPosition(loc, position)) {
+      nearestPos = i;
+    }
+  }
+
+  let endEnclosingPos = -1;
+  if (nearestPos >= 0) {
+    let parentLoc = locations[nearestPos];
+    for (let i = nearestPos + 1; i < locations.length; i++) {
+      let loc = locations[i];
+      if (containsLocation(parentLoc, loc)) {
+        endEnclosingPos = i;
+      }
+    }
+  }
+
+  console.log("TMP > nearestPos =", nearestPos);
+  console.log("TMP > endEnclosingPos =", endEnclosingPos);
+  if (nearestPos >= 0 && endEnclosingPos >= 0) {
+    let newLocs = locations.slice();
+    newLocs.splice(nearestPos + 1, endEnclosingPos - nearestPos);
+    return newLocs;
+  }
+  return locations;
+}
+
 /**
  * Returns an array of locations that are considered out of scope for the given
  * location.
@@ -80,12 +119,17 @@ function getOutOfScopeLocations(
   const { functions, comments } = findSymbols(source);
   const commentLocations = comments.map(c => c.location);
 
-  return functions
-    .map(getLocation)
-    .concat(commentLocations)
-    .filter(loc => !containsPosition(loc, position))
-    .reduce(removeOverlaps, [])
-    .sort(sortByStart);
+  console.log("");
+  console.log("TMP > position line =", position.line);
+  let ll = functions.map(getLocation);
+  console.log("TMP > ll =", ll);
+  ll = testtt(ll, position);
+  console.log("TMP > ll testtted =", ll);
+  ll = ll.filter(loc => !containsPosition(loc, position)); //ll.concat(commentLocations)
+  console.log("TMP > ll filtered =", ll);
+  ll = ll.reduce(removeOverlaps, []);
+  console.log("TMP > ll removed overlaps =", ll);
+  return ll.sort(sortByStart);
 }
 
 export default getOutOfScopeLocations;
